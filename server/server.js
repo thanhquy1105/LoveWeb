7347aca6409551f9442b7b01ce1d58889d07a826
Thành Quý <thanhquy1105@gmail.com>
2021-01-22 03:12:11 +0700

    update for heroku

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const app = require("./app");
+const path = require("path");
+const express = require("express");
 const connectDatabase = require("./config/database");
 const dotenv = require("dotenv");
 const { ImportShowDays } = require("./controllers/ShowDaysController");
@@ -6,8 +8,11 @@ const { ImportShowFull } = require("./controllers/ShowFullController");
 const { ImportAvatar } = require("./controllers/AvatarController");
 const PORT = process.env.PORT || 5000;
 
+const buildPath = path.join(__dirname, "..", "build");
+app.use(express.static(buildPath));
+
 //Setting up config file
-dotenv.config({ path: "backend/config/config.env" });
+dotenv.config({ path: "config/config.env" });
 
 //Connecting to database
 connectDatabase();

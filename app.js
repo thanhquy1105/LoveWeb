9823889915e3a22fa39108a56bbc356a5b9a7762
Thành Quý <thanhquy1105@gmail.com>
2021-01-22 23:59:27 +0700

    Update

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "./client/build")));
+app.use(express.static(path.join("client/build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join("./client/build/index.html"));
+  res.sendFile(path.join("client/build/index.html"));
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./client/build"));
+  app.use(express.static("client/build"));
 }
 
 //Import all routes

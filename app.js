946124080ee8db462dc6b8fc83e591a052d328b1
Thành Quý <thanhquy1105@gmail.com>
2021-01-23 14:24:18 +0700

    Update

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join("client/build")));
-app.get("*", (req, res) => {
-  res.sendFile(path.join("client/build/index.html"));
-});
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
 
 //Import all routes
 
 const info = require("./routes/info");
 
 app.use("/api", info);
+
+// If no API routes are hit, send the React app
+router.use(function (req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -5,12 +5,21 @@ const { ImportShowDays } = require("./controllers/ShowDaysController");
 const { ImportShowFull } = require("./controllers/ShowFullController");
 const { ImportAvatar } = require("./controllers/AvatarController");
 const PORT = process.env.PORT || 8000;
+const routes = require("./routes");
 
 //Setting up config file
 dotenv.config({ path: "config/config.env" });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 //Connecting to database
 connectDatabase();
+
+// ADD THIS LINE
+app.use(express.static("client/build"));
+
+app.use(routes);
 
 ImportShowDays();
 ImportShowFull();

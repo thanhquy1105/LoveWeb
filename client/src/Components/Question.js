6a704f6a1abb38d9830c53bd9e1c38ae35d413cf
Thành Quý <thanhquy1105@gmail.com>
2021-07-05 02:34:12 +0700

    Update Music

--- client/src/Components/Question.js
+++ client/src/Components/Question.js
@@ -6,6 +6,7 @@ import { Row, Col } from "antd";
 import "antd/dist/antd.css";
 import BrokenHeart from "../image/brokenHeart.png";
 import "./NoButton.css";
+import Music from "./music/La.mp3";
 
 var mystyle, x, y;
 class Question extends React.Component {
